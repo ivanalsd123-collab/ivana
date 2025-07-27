37045767e2f34211f6a358223ee5f6ed805d422e
leywinnyin <leywinmewing@gmail.com>
2025-07-27 22:32:32 +0700

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 const title = document.querySelector('.title')
-const text = `I Have Something`.split('')
+const text = `i have something for u`.split('')
 
 // Create container for better responsive layout
 title.style.display = 'flex'
